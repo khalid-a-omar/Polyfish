af2db416975535e4423b395ebf4e6f2e4a4f34e9
khalid-a-omar <khalid_omar@msn.com>
2022-02-26 19:28:07 +0400

    Fix windows build and add new line to end of script

--- Tools/build_win.bat
+++ Tools/build_win.bat
@@ -4,11 +4,10 @@ SETLOCAL
 SETLOCAL EnableDelayedExpansion
 SETLOCAL EnableExtensions
 
-SET MSYS_BIN=C:\msys64\usr\bin
-SET MINGW_PATH=C:\msys64\mingw64
-SET PATH=%MINGW_PATH%\bin;%MSYS_BIN%;%SDE_PATH%;%PATH%
-SET SDE_EXE=C:/SDE/sde-external-9.0.0-2021-11-07-win/sde.exe
-SET 7Z=C:\Program Files\7-Zip\7z.exe
+SET "MSYS_BIN=C:\msys64\usr\bin"
+SET "MINGW_PATH=C:\msys64\mingw64"
+SET "SDE_EXE=C:/SDE/sde-external-9.0.0-2021-11-07-win/sde.exe"
+SET "_7Z=C:\Program Files\7-Zip\7z.exe"
 
 :Main
 	CALL :Setup                       || (PAUSE & EXIT /B 1)
@@ -29,6 +28,9 @@ SET 7Z=C:\Program Files\7-Zip\7z.exe
 	EXIT /B 0
 
 :Setup
+	REM Path
+	SET PATH=%MINGW_PATH%\bin;%MSYS_BIN%;%SDE_PATH%;%PATH%
+	
 	REM Construct build date string as YYMMDD to be used in the exe name
 	FOR /F "usebackq skip=1 tokens=1-3" %%A IN (`wmic Path Win32_LocalTime Get Day^,Month^,Year ^| findstr /r /v "^$"`) DO (
 		SET DD=00%%A
@@ -80,7 +82,7 @@ SET 7Z=C:\Program Files\7-Zip\7z.exe
 	make.exe profile-build ARCH=%~1 COMP=mingw -j 4 || (PAUSE & EXIT /B 1)
 	strip.exe Polyfish.exe || EXIT /B 1
 	REN Polyfish.exe "Polyfish_%DateString%_%~1.exe" || (PAUSE & EXIT /B 1)
-	"%7Z%" a -tzip "%BUILD_DIR%\Polyfish_%DateString%_%~1.zip" "Polyfish_%DateString%_%~1.exe" || (PAUSE & EXIT /B 1)
+	"%_7Z%" a -tzip "%BUILD_DIR%\Polyfish_%DateString%_%~1.zip" "Polyfish_%DateString%_%~1.exe" || (PAUSE & EXIT /B 1)
 	make.exe clean || (PAUSE & EXIT /B 1)
 	ERASE "Polyfish_%DateString%_%~1.exe" || (PAUSE & EXIT /B 1)
 	POPD
