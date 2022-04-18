5d39ad42102f3e3a4da22d12cea558cf4438a7f7
Khalid Omar <khalid_omar@msn.com>
2022-04-18 20:11:59 +0400

    Merge with latest Stockfish development branch (Revision 8953)

--- src/uci.cpp
+++ src/uci.cpp
@@ -2,7 +2,7 @@
   Polyfish, a UCI chess playing engine derived from Stockfish
   Copyright (C) 2004-2022 The Polyfish developers (see AUTHORS file)
 
-  Polyfish is free software: you can redistribute it and/or modify
+  Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
